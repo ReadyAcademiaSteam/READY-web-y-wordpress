ef204d074442f8cbdcc0bfeeddb3dbfe4108eb99
ReadyDev <academiaready@gmail.com>
2019-08-04 02:38:43 +0200

    video quienes somos

--- web/quienes-somos.php
+++ web/quienes-somos.php
@@ -36,25 +36,69 @@
 <meta name="theme-color" content="#ffffff">
 
 </head>
-<body>
+<body data-icon="12">
 
 <div id="wrapper">
 
 <? include("include/cabecera.php"); ?>
 
-<section id="page-title" class="page-title-center background-overlay-dark text-light" style="background-image:url(images/parallax/2.jpg);">
-  <div class="container">
-    <div class="page-title">
-      <h1>Quiénes somos</h1>
+<section id="page-title" class="page-title-center text-light" data-vide-bg="video/explore/explore" data-vide-options="position: 0% 50%">
+    <div style="position: absolute; z-index: -1; top: 0px; left: 0px; bottom: 0px; right: 0px; overflow: hidden; background-size: cover; background-color: #1c1c1c; background-repeat: no-repeat; background-position: 0% 50%; background-image: none;">
+        <video autoplay="" loop="" muted="" style="margin: auto; position: absolute; z-index: -1; top: 50%; left: 0%; transform: translate(0%, -50%); visibility: visible; opacity: 0.6; width: 100%;">
+        <source src="images/slider/quienes-somos-video.mp4" type="video/mp4">
     </div>
-    <div class="breadcrumb">
-      <ul>
-        <li><a href="#">Inicio</a></li>
-        <li><a href="#">Sobre nosotros</a></li>
-        <li class="active"><a href="#">Quiénes somos</a></li>
-      </ul>
+    <div class="container">
+        <div class="breadcrumb">
+            <ul>
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">Page title</a> </li>
+                <li class="active"><a href="#">Video background</a> </li>
+            </ul>
+        </div>
+        <div class="page-title">
+            <h1>Quienes somos</h1>
+            <span>Gente joven enamorada de su profesión: enseñar.</span>
+        </div>
     </div>
-  </div>
+</section>
+
+<section data-animation="fadeInUp">
+<div class="container">
+<div class="row">
+<div class="col-lg-3">
+<div class="heading-text heading-section">
+<h2>LA COMPAÑÍA</h2>
+</div>
+</div>
+<div class="col-lg-9">
+<div class="row">
+<div class="col-lg-6">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, <br> <br> facilisis ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </div>
+<div class="col-lg-6">Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. <br> <br>The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volut.</div>
+</div></div>
+</div>
+</div>
+</section>
+
+<section class="box-fancy section-fullwidth text-light p-b-0">
+    <div class="row">
+        <div style="background-color:#ed6d6b" class="col-lg-4" >
+        <h1 class="text-lg text-uppercase">01.</h1>
+        <h3>Refuerzo escolar</h3>
+        <span data-animation="fadeIn">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
+        </div>
+
+        <div style="background-color:#5994aa" class="col-lg-4">
+        <h1 class="text-lg text-uppercase">02.</h1>
+        <h3>Tecnología</h3>
+        <span data-animation="fadeIn" style="animation-delay: 0.25s">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
+        </div>
+
+        <div style="background-color:#ffdf66" class="col-lg-4">
+        <h1 class="text-lg text-uppercase">03.</h1>
+        <h3>Idiomas</h3>
+        <span data-animation="fadeIn" style="animation-delay: 0.5s">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
+        </div>
+    </div>
 </section>
 
 <section class="p-b-0">
@@ -65,44 +109,6 @@
         <h3>Y amamos nuestra profesión.</h3>
     </div>
 
-       
-<div class="container">
-<div class="row">
-<div class="col-lg-3">
-<div class="heading-text heading-section">
-<h2>THE COMPANY</h2>
-</div>
-</div>
-<div class="col-lg-9">
-<div class="row">
-<div class="col-lg-6">The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, <br> <br> facilisis ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. </div>
-<div class="col-lg-6">Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. Fusce id mi diam, non ornare orci. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. Pellentesque ipsum erat, facilisis ut venenatis eu, sodales vel dolor. <br> <br>The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volut.</div>
-</div></div>
-</div>
-</div>
-</div>
-</section>
-<section class="box-fancy section-fullwidth text-light p-b-0">
-<div class="row">
-<div style="background-color:#917d5d" class="col-lg-4">
-<h1 class="text-lg text-uppercase">01.</h1>
-<h3>CONCEPT</h3>
-<span>The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
-</div>
-<div style="background-color:#a08c6c" class="col-lg-4">
-<h1 class="text-lg text-uppercase">02.</h1>
-<h3>DEVELOPMENT</h3>
-<span>The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
-</div>
-<div style="background-color:#ad9979" class="col-lg-4">
-<h1 class="text-lg text-uppercase">03.</h1>
-<h3>TESTING</h3>
-<span>The most happiest time of the day!. Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida tortor, vel interdum mi sapien ut justo. Nulla varius consequat magna, id molestie ipsum volutpat quis. A true story, that never been told!. </span>
-</div>
-</div>
-</section>
-
-    <section>
       <div class="row team-members team-members-circle m-b-40">
         <!-- Antonio Reverte -->
         <div class="col-lg-4" data-animation="fadeInLeft">
@@ -199,6 +205,137 @@
         </div>
 
   </div>
+</section>
+
+<section class="background-grey">
+<div class="container">
+<div class="heading-text heading-section text-center">
+<h2>NUESTRO EQUIPO</h2>
+<span class="lead">Sin ellos, no seríamos nada.</span>
+</div>
+<div class="row team-members team-members-shadow m-b-40">
+<div class="col-lg-3">
+<div class="team-member">
+<div class="team-image">
+<img src="images/team/6.jpg">
+</div>
+<div class="team-desc">
+<h3>Alba Jimenez</h3>
+<span>Software Developer</span>
+<p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
+<div class="align-center">
+<a class="btn btn-xs btn-slide btn-light" href="#">
+<i class="fab fa-facebook-f"></i>
+<span>Facebook</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+<i class="fab fa-twitter"></i>
+<span>Twitter</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+<i class="fab fa-instagram"></i>
+<span>Instagram</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+<i class="far fa-envelope"></i>
+<span>Mail</span>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="team-member">
+<div class="team-image">
+<img src="images/team/7.jpg">
+</div>
+<div class="team-desc">
+<h3>Victor Martinez</h3>
+<span>Software Developer</span>
+<p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
+<div class="align-center">
+<a class="btn btn-xs btn-slide btn-light" href="#">
+<i class="fab fa-facebook-f"></i>
+<span>Facebook</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+<i class="fab fa-twitter"></i>
+<span>Twitter</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+<i class="fab fa-instagram"></i>
+<span>Instagram</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+<i class="far fa-envelope"></i>
+<span>Mail</span>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="team-member">
+<div class="team-image">
+<img src="images/team/8.jpg">
+</div>
+<div class="team-desc">
+<h3>Lucia Cabañas</h3>
+<span>Software Developer</span>
+<p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
+<div class="align-center">
+<a class="btn btn-xs btn-slide btn-light" href="#">
+<i class="fab fa-facebook-f"></i>
+<span>Facebook</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+<i class="fab fa-twitter"></i>
+<span>Twitter</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+<i class="fab fa-instagram"></i>
+<span>Instagram</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+<i class="far fa-envelope"></i>
+<span>Mail</span>
+</a>
+</div>
+</div>
+</div>
+</div>
+<div class="col-lg-3">
+<div class="team-member">
+<div class="team-image">
+<img src="images/team/9.jpg">
+</div>
+<div class="team-desc">
+<h3>Maria Nieto</h3>
+<span>Software Developer</span>
+<p>The most happiest time of the day!. Praesent tristique hendrerit ex ut laoreet. </p>
+<div class="align-center">
+<a class="btn btn-xs btn-slide btn-light" href="#">
+<i class="fab fa-facebook-f"></i>
+<span>Facebook</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="100">
+<i class="fab fa-twitter"></i>
+<span>Twitter</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="#" data-width="118">
+<i class="fab fa-instagram"></i>
+<span>Instagram</span>
+</a>
+<a class="btn btn-xs btn-slide btn-light" href="mailto:#" data-width="80">
+<i class="far fa-envelope"></i>
+<span>Mail</span>
+</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 </section>
 
 <? include("include/pie.php"); ?>
